c3c9588df0e11f6ade9616314f61d66a629190f4
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-07 19:13:15 -0400

    Render TransactionList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses'
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </>
   );
