7a4cfeb1c7257d8c0d47d923a1937a8405d58206
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-07 19:07:13 -0400

    Render Balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Header} from './components/Header';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
 import './App.css';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </>
   );
 }
 
