521d931ae1d3601f526c19b3726ec37d19126021
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-07 19:36:09 -0400

    Create GlobalContext Provider

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
 // Initial state
 const initialState = {
@@ -12,3 +13,15 @@ const initialState = {
 
 // Create context
 export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState); 
+
+    return (
+    <GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
