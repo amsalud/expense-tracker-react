25343e645dc7edb7eed262dd0b1f307af97bbde2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-07 20:34:52 -0400

    Add onSubmit method to handle form submition and add Transaction data to GlobalState

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionForm = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = e => {
+      e.preventDefault();
+
+      const newTransaction = {
+        id: Math.floor(Math.random() * 100000000),
+        amount: +amount,
+        text
+      };
+
+      addTransaction(newTransaction);
+      
+      setText('');
+      setAmount(0);
+    }
     
     return (
         <>
         <h3>Add new transaction</h3>
-        <form id="form">
+        <form id="form" onSubmit={onSubmit}>
           <div className="form-control">
             <label htmlFor="text">Text</label>
             <input type="text" value={text} onChange={(e)=>{setText(e.target.value)}} id="text" placeholder="Enter text..." />
