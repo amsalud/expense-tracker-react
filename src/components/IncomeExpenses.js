8e79d889fe76b2bbdc6224508585ab5c67efadb5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-07 20:08:00 -0400

    Display Income and Expenses

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const income = transactions.filter(item => item.amount > 0).reduce((acc, item)=> (acc += item.amount), 0).toFixed(2);
+    const expenses = (transactions.filter(item => item.amount < 0).reduce((acc, item) => (acc += item.amount), 0) * -1).toFixed(2); 
+
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p id="money-plus" className="money plus">+$0.00</p>
+                <p id="money-plus" className="money plus">+${income}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p id="money-minus" className="money minus">-$0.00</p>
+                <p id="money-minus" className="money minus">-${expenses}</p>
             </div>
       </div>
     )
